df9f7345417e2ba83043160fddccb0695cc079d5
2daw <2daw@T4-PC20>
2015-01-27 20:50:01 +0100

    Hechos los frames y generador de cartón en proceso

--- public_html/funciones.js
+++ public_html/funciones.js
@@ -1,9 +1,12 @@
 /*********************************
-Autor: Alamillo Arroyo, Sara
-Fecha creación: 27/01/2015
-Última modificación: 27/01/2015
-Versión: 1.00
-*********************************/
+ Autor: Alamillo Arroyo, Sara
+ Fecha creación: 27/01/2015
+ Última modificación: 27/01/2015
+ Versión: 1.00
+ *********************************/
+
+var numJugadores;
+var valCarton;
 
 // Asigna eventos a los diferentes objetos
 var crearEvento = function () {
@@ -16,3 +19,38 @@ var crearEvento = function () {
     }
 
 }();
+
+function validarJugadores() {
+    var valor = document.getElementById("numJugadores").value;
+    if (valor < 5 || valor > 20) {
+        document.getElementById("numJugadores").value = "";
+    }
+}
+
+function validarValor() {
+    var valor = document.getElementById("valCarton").value;
+    if (valor < 1 || valor > 5) {
+        document.getElementById("valCarton").value = "";
+    }
+}
+
+function iniciarBingo() {
+    numJugadores = document.getElementById("numJugadores").value;
+    valCarton = document.getElementById("valCarton").value;
+    dibujarCarton();
+    alert("ok");
+}
+function dibujarCarton() {
+    var carton = document.createElement("table");
+   var ventana = document.getElementsByName("frameCarton")[0].body;
+   alert(ventana.getElementById("prueba").innerHTML);
+    for (var i = 0, max = 3; i < max; i++) {
+        var columna = document.createElement("tr");
+        for (var i = 0, max = 9; i < max; i++) {
+            var celda = document.createElement("td");
+            columna.appendChild(celda);
+        }        
+        carton.appendChild(columna);
+    }
+    ventana.appendChild(carton);
+}
