edd7ce774d9faac5313cab5ff47ea256149b40f6
Sara <Sara@portatil>
2015-02-01 12:51:20 +0100

    Haciendo el cartón del bingo. Fallan los números aleatorios.

--- public_html/funciones.js
+++ public_html/funciones.js
@@ -38,13 +38,47 @@ function iniciarBingo() {
 }
 function dibujarCarton() {
     var carton = document.createElement("table");
-    carton.setAttribute("border", "1");
+    carton.setAttribute("id", "carton");
    var capa = document.getElementById("ladoDerecho");
+   
   
     for (var i = 0, max = 3; i < max; i++) {
         var columna = document.createElement("tr");
-        for (var j = 0, max = 9; j < max; j++) {
+        for (var j = 0, max2 = 9; j < max2; j++) {
+	    var numAleatorio;
+	    switch(j) {
+		case 0:
+		    numAleatorio = Math.floor((Math.random() * 9) + 1); 
+		    break;
+		case 1:
+		    numAleatorio = Math.floor((Math.random() * 19) + 10); 
+		    break;
+		case 2:
+		    numAleatorio = Math.floor((Math.random() * 29) + 20); 
+		    break;
+		case 3:
+		    numAleatorio = Math.floor((Math.random() * 39) + 30); 
+		    break;
+		case 4:
+		    numAleatorio = Math.floor((Math.random() * 49) + 40); 
+		    break;
+		case 5:
+		    numAleatorio = Math.floor((Math.random() * 59) + 50); 
+		    break;
+		case 6:
+		    numAleatorio = Math.floor((Math.random() * 69) + 60); 
+		    break;
+		case 7:
+		    numAleatorio = Math.floor((Math.random() * 79) + 70); 
+		    break;
+		case 8:
+		    numAleatorio = Math.floor((Math.random() * 90) + 80); 
+		    break;
+	    }
             var celda = document.createElement("td");
+	    var texto = document.createTextNode(numAleatorio);
+	    celda.appendChild(texto);
+	    celda.classList.add("numero");
             columna.appendChild(celda);
         }        
         carton.appendChild(columna);
